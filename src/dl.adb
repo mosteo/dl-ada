b805f48e938d369f17a6d2627be96fa49157e477
Alejandro R. Mosteo <amosteo@unizar.es>
2021-03-15 14:33:27 +0100

    Safer functions with checked results

--- src/dl.adb
+++ src/dl.adb
@@ -1,4 +1,63 @@
+with C_Strings;
+
+with Dlx.Dlfcn_H;
+
+with Interfaces.C.Strings;
+
 package body DL is
+
+   package Bind renames Dlx.Dlfcn_H;
+   package CS   renames Interfaces.C.Strings;
+
+   use Interfaces;
+
+   ----------
+   -- Open --
+   ----------
+
+   function Open (File  : String   := "";
+                  Mode  : Modes    := Now;
+                  Flags : Or_Flags := 0) return Handle is
+   begin
+      return Result : constant Handle :=
+        Handle
+          (Bind.Dlopen
+             (Uu_File => (if File /= ""
+                          then C_Strings.To_C (File).To_Ptr
+                          else CS.Null_Ptr),
+              Uu_Mode => C.Int ((if Mode = Lazy
+                                 then Defs.RTLD_LAZY
+                                 else Defs.RTLD_NOW) + Flags)))
+      do
+         if File /= "" and then System.Address (Result) in System.Null_Address
+         then
+            raise Symbol_Error with "[DL.Open] Failed to open: " & File;
+         end if;
+      end return;
+   end Open;
+
+   ---------
+   -- Sym --
+   ---------
+
+   function Sym (H    : Handle;
+                 Name : String) return System.Address is
+   begin
+      return Result : constant System.Address :=
+        Bind.Dlsym (Uu_Handle => System.Address (H),
+                    Uu_Name   => C_Strings.To_C (Name).To_Ptr)
+      do
+         if Result in System.Null_Address then
+            if System.Address (H) in System.Null_Address then
+               raise Symbol_Error
+                 with "[DL.Sym] Symbol not found in main program: " & Name;
+            else
+               raise Symbol_Error
+                 with "[DL.Sym] Symbol not found in given handle: " & Name;
+            end if;
+         end if;
+      end return;
+   end Sym;
 
    -----------
    -- Error --
--- src/dl.ads
+++ src/dl.ads
@@ -1,15 +1,12 @@
 with Ada.Unchecked_Conversion;
 
-with C_Strings;
-
-with Interfaces.C.Strings;
+with DLx.X86_64_Linux_Gnu_Bits_Dlfcn_H;
 
 with System;
 
-with DLx.Dlfcn_H;
-with DLx.X86_64_Linux_Gnu_Bits_Dlfcn_H;
-
 package DL is
+
+   Symbol_Error : exception;
 
    type Handle is private;
 
@@ -28,9 +25,11 @@ package DL is
    function Open (File  : String   := "";
                   Mode  : Modes    := Now;
                   Flags : Or_Flags := 0) return Handle;
+   --  If File /= "" and Open'Result would be null, Symbol_Error is raised
 
    function Sym (H    : Handle;
                  Name : String) return System.Address;
+   --  Raises Symbol_Error when symbol not found (so it never returns null)
 
    function Error return String;
    --  Returns last error or empty string
@@ -39,11 +38,7 @@ private
 
    pragma Linker_Options ("-ldl");
 
-   package Bind renames DLx.Dlfcn_H;
    package Defs renames DLx.X86_64_Linux_Gnu_Bits_Dlfcn_H;
-
-   package C  renames Interfaces.C;
-   package CS renames Interfaces.C.Strings;
 
    type Handle is new System.Address;
 
@@ -56,28 +51,5 @@ private
    RTLD_GLOBAL       : constant Or_Flags := Defs.RTLD_GLOBAL;
    RTLD_LOCAL        : constant Or_Flags := Defs.RTLD_LOCAL;
    RTLD_NODELETE     : constant Or_Flags := Defs.RTLD_NODELETE;
-
-
-
-   ----------
-   -- Open --
-   ----------
-
-   function Open (File  : String   := "";
-                  Mode  : Modes    := Now;
-                  Flags : Or_Flags := 0) return Handle is
-     (Handle
-        (Bind.Dlopen
-             ((if File /= "" then C_Strings.To_C (File).To_Ptr else CS.Null_Ptr),
-              C.Int((if Mode = Lazy then Defs.RTLD_LAZY else Defs.RTLD_NOW) + Flags))));
-
-   ---------
-   -- Sym --
-   ---------
-
-   function Sym (H    : Handle;
-                 Name : String) return System.Address is
-     (Bind.Dlsym (System.Address (H),
-                  C_Strings.To_C (Name).To_Ptr));
 
 end DL;
